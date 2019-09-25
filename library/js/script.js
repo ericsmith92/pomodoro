f9f6e8d795004b20da037dfd151d35c073ae17f2
ericsmith92 <ericsmth884@gmail.com>
2019-09-25 16:14:55 -0400

    barebones in place, still a little buggy

--- library/js/script.js
+++ library/js/script.js
@@ -3,11 +3,17 @@ function start(){
     const timerMins = document.querySelector('.timer_minutes');
     const timerSecs = document.querySelector('.timer_seconds');
     const ding = document.querySelector('.timer_audio');
+    const title = document.querySelector('title');
 
-    let timeTarget = new Date().getTime() + 30000;
-    let workCycle = true;
+    //let timeTarget = 
+    //let workCycle = true;
 
-    function startTimerWork(){
+    function startTimer(){
+        const kickOffTime = new Date().getTime() + 1500000;
+        timer(kickOffTime, true);
+    }
+
+    const timer = (timeTarget, workCycle) => {
        setInterval (function() {
             
             const timeNow = new Date().getTime();
@@ -16,20 +22,27 @@ function start(){
             const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
             const seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
-            timerSecs.innerHTML = seconds;
-            timerMins.innerHTML = minutes;
+            console.log(typeof minutes);
+
+            timerSecs.innerHTML = seconds  <= 9 ? `0${seconds}` : seconds;
+            timerMins.innerHTML = minutes <= 9 ? `0${minutes}` : minutes;
+            title.innerHTML = `${minutes}:${seconds}`;
 
             if(difference <= 0){
+                clearInterval(timer);
+                timerSecs.innerHTML = '--';
+                timerMins.innerHTML = '--';
+                title.innerHTML = '--:--';
                 ding.play();
-                timeTarget = workCycle ? new Date().getTime() + 5000 : new Date().getTime() + 10000;
+                timeTarget = workCycle ? new Date().getTime() + 300000 : new Date().getTime() +  1500000;
                 workCycle = workCycle ? false : true;
-                startTimerWork();
+                timer(timeTarget, workCycle);
             }
 
         }, 1000);
     }
 
-    startBtn.addEventListener('click', startTimerWork);
+    startBtn.addEventListener('click', startTimer);
 }
 
 window.onload = start;
