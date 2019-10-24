36393e0cff5ea56e75689e16b9d21f12c6f5643b
ericsmith92 <ericsmth884@gmail.com>
2019-10-24 16:58:12 -0400

    fixing interval clearing issue, partially done

--- library/js/script.js
+++ library/js/script.js
@@ -6,16 +6,34 @@ function start(){
     const title = document.querySelector('title');
  
     function startTimer(){
-        const kickOffTime = new Date().getTime() + 1500000;
+        //const kickOffTime = new Date().getTime() + 1500000;
+        const kickOffTime = new Date().getTime() + 20000;
         timer(kickOffTime, true);
     }
 
-    const timer = (timeTarget, workCycle) => {
-       setInterval (function() {
-            
-            const timeNow = new Date().getTime();
-            const difference = timeTarget - timeNow;
+    let eric = null;
 
+    const timer = (timeTarget, workCycle) => {
+        
+       eric = setInterval (function() {
+        
+        const timeNow = new Date().getTime();
+        const difference = timeTarget - timeNow;
+        console.log(difference);
+        if(difference <= 0){
+            console.log('fired');
+            clearInterval(eric);
+            timerSecs.innerHTML = '--';
+            timerMins.innerHTML = '--';
+            title.innerHTML = '--:--';
+            ding.play();
+            //timeTarget = workCycle ? new Date().getTime() + 300000 : new Date().getTime() +  1500000;
+            timeTarget = workCycle ? new Date().getTime() + 10000 : new Date().getTime() +  20000;
+            workCycle = workCycle ? false : true;
+            timer(timeTarget, workCycle);
+        }
+            
+            
             let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
             let seconds = Math.floor((difference % (1000 * 60)) / 1000);
 
@@ -25,17 +43,6 @@ function start(){
             timerMins.innerHTML = minutes;
             timerSecs.innerHTML = seconds;
             title.innerHTML = `${minutes}:${seconds}`;
-
-            if(difference <= 0){
-                clearInterval(timer);
-                timerSecs.innerHTML = '--';
-                timerMins.innerHTML = '--';
-                title.innerHTML = '--:--';
-                ding.play();
-                timeTarget = workCycle ? new Date().getTime() + 300000 : new Date().getTime() +  1500000;
-                workCycle = workCycle ? false : true;
-                timer(timeTarget, workCycle);
-            }
 
         }, 1000);
     }
